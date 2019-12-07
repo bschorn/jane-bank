98165360167a43c5393841f9ddb2b1dc5312202b
Bryan Schorn <bschorn@gmail.com>
2019-12-07 00:11:17 -0500

    Checkin

--- src/main/java/com/schorn/jane/bank/Main.java
+++ src/main/java/com/schorn/jane/bank/Main.java
@@ -6,10 +6,45 @@
  */
 package com.schorn.jane.bank;
 
+import org.schorn.ella.ComponentProperties;
+import org.schorn.ella.app.ActiveMain;
+import org.schorn.ella.app.NodeConfig;
+import org.schorn.ella.schema.ActiveSchemaParser;
+import org.schorn.ella.ws.EllaWsApplication;
+
 /**
  *
  * @author bschorn
  */
 public class Main {
+
+    public static void main(String[] args) {
+        try {
+            /*
+                Running InterNodal
+             */
+            ActiveMain.Starter starter = new ActiveMain.Starter(args).create();
+
+            String context = "jane_bank";
+            String specFile = ComponentProperties.NODE.getProperty("ActiveSpec", null);
+            String[] metaFiles = NodeConfig.ACTIVE_METAS.values(",");
+            if (specFile != null) {
+                ActiveSchemaParser.compile(context, specFile, metaFiles[0]);
+            }
+            starter.start();
+
+            /*
+                Connectivity
+             */
+            new EllaWsApplication.Starter(args).create().start();
+            /* ---- alternatively -----
+            Starter starter = new Starter(args).create();
+            // Do something else first
+            starter.start();
+             */
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
 }
